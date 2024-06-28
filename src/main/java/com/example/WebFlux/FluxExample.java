08667c83ec788c0fc4183636b0675ac9d9f09442
Andres Perez <perezandres1799@gmail.com>
2024-06-28 08:18:30 -0500

    Example with flux

---
+++ src/main/java/com/example/WebFlux/FluxExample.java
@@ -0,0 +1,17 @@
+package com.example.WebFlux;
+
+import reactor.core.publisher.Flux;
+
+public class FluxExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Flux <String> flux= Flux.fromArray(new String[] {"Andres","Arturo","Perez"});
+		flux.subscribe(System.out::println);
+		
+		flux.doOnNext(System.out::println).subscribe();
+
+	}
+
+}
