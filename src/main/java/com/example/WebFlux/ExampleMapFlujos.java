9fdbdaf0d0eb80f1b87a373a9fdf1fd79ce36669
Andres Perez <perezandres1799@gmail.com>
2024-07-05 15:48:59 -0500

    Example with map

---
+++ src/main/java/com/example/WebFlux/ExampleMapFlujos.java
@@ -0,0 +1,16 @@
+package com.example.WebFlux;
+
+import reactor.core.publisher.Flux;
+
+public class ExampleMapFlujos {
+
+	public static void main(String[] args) {
+	
+		
+		Flux.fromArray(new String[] {"Tom","Juan","Aura","Diana"})
+		                            .map(String::toUpperCase)
+		                            .subscribe(System.out::println);
+
+	}
+
+}
