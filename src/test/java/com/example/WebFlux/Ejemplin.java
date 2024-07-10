79e02e4c26f9492ed2b60c953cfb013740f2ce70
Andres Perez <perezandres1799@gmail.com>
2024-07-10 15:00:36 -0500

    Merge

--- src/test/java/com/example/WebFlux/Ejemplin.java
+++ src/test/java/com/example/WebFlux/Ejemplin.java
@@ -3,12 +3,12 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
-
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 
 public class Ejemplin {
-
 	
 	@Test
 	public void testTransMap() {
@@ -18,5 +18,25 @@ public class Ejemplin {
 				                 .filter(nombre -> nombre.length()>5)
 				                 .map(nombre->nombre.toUpperCase())
 				                 .log();
+		
+		 StepVerifier.create(nombresFlux)
+         .expectNext("GOOGLE","STACKOVERFLOW")
+         .verifyComplete();
 	}
+	
+	  @Test
+	    public void testTransformUsingFlatMap(){
+	        List<String> listaNombres = Arrays.asList("google","abc","fb","stackoverflow");
+	        Flux<String> nombresFlux = Flux.fromIterable(listaNombres)
+	                .filter(nombre -> nombre.length() > 5)
+	                .flatMap(nombre -> {
+	                    return Mono.just(nombre.toUpperCase());
+	                })
+	                .log();
+
+	        StepVerifier.create(nombresFlux)
+	                .expectNext("GOOGLE","STACKOVERFLOW")
+	                .verifyComplete();
+	    }
+
 }
